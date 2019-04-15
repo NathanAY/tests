4a394334a33848d68f0a2dcf9ecc70bd163f4d5e
Yanchevsky, Artyom <a.yanchevsky@itransition.com>
2019-04-15 16:01:42 +0300

    sout feature

--- src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ7.java
+++ src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ7.java
@@ -1,4 +1,7 @@
 package com.atlassian.jira.bc.issue.worklog;
 
 public class ClassJ7 {
+    public void test1 () {
+        System.out.println("1 1");
+    }
 }
--- src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ71.java
+++ src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ71.java
@@ -1,4 +1,7 @@
 package com.atlassian.jira.bc.issue.worklog;
 
 public class ClassJ71 {
+    public void test1 () {
+        System.out.println("1");
+    }
 }
