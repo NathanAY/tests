8242d90d03ff6185eef3d2458cf427c0fb81354e
Yanchevsky, Artyom <a.yanchevsky@itransition.com>
2019-04-15 16:27:20 +0300

    100% final

--- src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ71.java
+++ src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ71.java
@@ -22,7 +22,7 @@ public class ClassJ71 {
     }
 
     public void addSuperFeature() {
-        System.out.println("test");
         System.out.println("50%");
+        System.out.println("100%");
     }
 }
