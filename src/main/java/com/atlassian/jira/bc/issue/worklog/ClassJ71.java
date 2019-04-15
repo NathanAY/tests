3b0a2bc20401fd09ce16b7df85d21b9ea1194a3f
Yanchevsky, Artyom <a.yanchevsky@itransition.com>
2019-04-15 16:22:32 +0300

    sout feature

--- src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ71.java
+++ src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ71.java
@@ -2,6 +2,6 @@ package com.atlassian.jira.bc.issue.worklog;
 
 public class ClassJ71 {
     public void changed() {
-        System.out.println("in j 8");
+        System.out.println("1");
     }
 }
