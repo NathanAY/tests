a9d7ba1f15cef969c188e255fbda92f190a90573
Yanchevsky, Artyom <a.yanchevsky@itransition.com>
2019-04-15 16:03:15 +0300

    feature 2

--- src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ7.java
+++ src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ7.java
@@ -3,5 +3,16 @@ package com.atlassian.jira.bc.issue.worklog;
 public class ClassJ7 {
     public void test1 () {
         System.out.println("1 1");
+        System.out.println("1 1");
+        System.out.println("1 1");
+        System.out.println("1 1");
+    }
+
+    public void test2 () {
+        System.out.println("1 1 2");
+    }
+
+    public void test3 () {
+        System.out.println("1 1 3");
     }
 }
--- src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ71.java
+++ src/main/java/com/atlassian/jira/bc/issue/worklog/ClassJ71.java
@@ -3,5 +3,21 @@ package com.atlassian.jira.bc.issue.worklog;
 public class ClassJ71 {
     public void test1 () {
         System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("123");
+        System.out.println("123");
+    }
+
+    public void test2() {
+        System.out.println("1");
+    }
+
+    public void test3() {
+        System.out.println("1");
+    }
+
+    public void test4() {
+        System.out.println("1");
     }
 }
